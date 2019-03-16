c4096c3c9378c9513b1699eaa480ebb5fb9e3bfa
0xSha512 <48031448+0xSha512@users.noreply.github.com>
2019-03-16 02:03:04 +0100

    Add files via upload

---
+++ zentalk-worker.js
@@ -0,0 +1,48 @@
+self.window = self 
+
+
+self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js');
+
+let crypt = null
+let privateKey = null
+
+
+onmessage = function(e) {
+  const [ messageType, messageId, text, key ] = e.data
+  let result
+  switch (messageType) {
+    case 'generate-keys':
+      result = generateKeypair()
+      break
+    case 'encrypt':
+      result = encrypt(text, key)
+      break
+    case 'decrypt':
+      result = decrypt(text)
+      break
+  }
+
+
+  postMessage([ messageId, result ])
+}
+
+
+function generateKeypair () {
+  crypt = new JSEncrypt({default_key_size: 2056})
+  privateKey = crypt.getPrivateKey()
+
+
+  return crypt.getPublicKey()
+}
+
+
+function encrypt (content, publicKey) {
+  crypt.setKey(publicKey)
+  return crypt.encrypt(content)
+}
+
+
+function decrypt (content) {
+  crypt.setKey(privateKey)
+  return crypt.decrypt(content)
+}
