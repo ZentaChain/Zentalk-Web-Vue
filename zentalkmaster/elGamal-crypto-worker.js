23a95ead205341b36e958c5c8c533449b7218f2d
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-04-14 02:19:12 +0200

    Add files via upload

---
+++ zentalkmaster/elGamal-crypto-worker.js
@@ -0,0 +1,62 @@
+self.window = self 
+
+
+self.importScripts('https://cdn.patricktriest.com/vendor/sjcl/sjcl.min.js');
+
+let keypair = null
+
+
+
+onmessage = function (e) {
+    
+    const [message_type, text, key] = e.data
+    let result
+
+
+    switch (message_type) {
+        case 'generate-keys':
+            result = generateKeypair()
+            break
+        case 'encrypt':
+            result = encrypt(text, key)
+            break
+        case 'decrypt':
+            result = decrypt(text)
+            break
+    }
+
+    
+    postMessage([message_type, result]);
+}
+
+
+function generateKeypair() {
+    keypair = sjcl.ecc.elGamal.generateKeys(256)
+
+
+    return serializePublicKey(keypair.pub.get())
+}
+
+function encrypt(content, publicKey) {
+    publicKey = unserializePublicKey(publicKey)
+    return sjcl.encrypt(publicKey, content)
+}
+
+
+
+function decrypt(content) {
+    return sjcl.decrypt(keypair.sec, content)
+}
+
+
+function serializePublicKey(key) {
+    return sjcl.codec.base64.fromBits(key.x.concat(key.y))
+}
+
+
+function unserializePublicKey(key) {
+    return new sjcl.ecc.elGamal.publicKey(
+        sjcl.ecc.curves.c256,
+        sjcl.codec.base64.toBits(key)
+    )
+}
