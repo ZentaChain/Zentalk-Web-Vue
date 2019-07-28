bfd631038164c4f9bac021c40724c364ca960dbb
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-07-28 02:36:04 +0200

    Update zentalk-worker.js

--- zentalk-worker.js
+++ zentalk-worker.js
@@ -1,6 +1,5 @@
 self.window = self 
 
-
 self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js');
 
 let crypt = null
@@ -22,25 +21,19 @@ onmessage = function(e) {
       break
   }
 
-
   postMessage([ messageId, result ])
 }
-
 
 function generateKeypair () {
   crypt = new JSEncrypt({default_key_size: 2056})
   privateKey = crypt.getPrivateKey()
-
-
   return crypt.getPublicKey()
 }
-
 
 function encrypt (content, publicKey) {
   crypt.setKey(publicKey)
   return crypt.encrypt(content)
 }
-
 
 function decrypt (content) {
   crypt.setKey(privateKey)
