f14adfdd2621b4ee1cf30797590a6914cbda1ef2
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-12-16 17:49:54 +0100

    Update zentalk-worker.js

--- zentalk-worker.js
+++ zentalk-worker.js
@@ -1,14 +1,11 @@
-self.window = self 
-
-
+self.window = self
 self.importScripts('https://cdnjs.cloudflare.com/ajax/libs/jsencrypt/2.3.1/jsencrypt.min.js');
 
 let crypt = null
 let privateKey = null
 
-
-onmessage = function(e) {
-  const [ messageType, messageId, text, key ] = e.data
+onmessage = function (e) {
+  const [messageType, messageId, text, key] = e.data
   let result
   switch (messageType) {
     case 'generate-keys':
@@ -21,28 +18,23 @@ onmessage = function(e) {
       result = decrypt(text)
       break
   }
-
-
-  postMessage([ messageId, result ])
+  postMessage([messageId, result])
 }
 
-
-function generateKeypair () {
-  crypt = new JSEncrypt({default_key_size: 4096})
+function generateKeypair() {
+  crypt = new JSEncrypt({
+    default_key_size: 2056
+  })
   privateKey = crypt.getPrivateKey()
-
-
   return crypt.getPublicKey()
 }
 
-
-function encrypt (content, publicKey) {
+function encrypt(content, publicKey) {
   crypt.setKey(publicKey)
   return crypt.encrypt(content)
 }
 
-
-function decrypt (content) {
+function decrypt(content) {
   crypt.setKey(privateKey)
   return crypt.decrypt(content)
 }
