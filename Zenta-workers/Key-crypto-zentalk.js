68db1c8b3951436d2b8db5b88004742408834b23
Harun Kacemer <40530024+ZentaChainAdmin@users.noreply.github.com>
2019-12-16 17:53:00 +0100

    Update Key-crypto-zentalk.js

--- Zenta-workers/Key-crypto-zentalk.js
+++ Zenta-workers/Key-crypto-zentalk.js
@@ -1,7 +1,8 @@
 self.window = self 
+
 let keypair = null
 
-onmessage = function (e) {
+onmessage = function (e) { 
     const [message_type, text, key] = e.data
     let result
     switch (message_type) {
@@ -18,22 +19,31 @@ onmessage = function (e) {
     postMessage([message_type, result]);
 }
 
+
 function generateKeypair() {
     keypair = sjcl.ecc.elGamal.generateKeys(256)
+
+
     return serializePublicKey(keypair.pub.get())
 }
+
 
 function encrypt(content, publicKey) {
     publicKey = unserializePublicKey(publicKey)
     return sjcl.encrypt(publicKey, content)
 }
 
+
 function decrypt(content) {
     return sjcl.decrypt(keypair.sec, content)
 }
+
+
 function serializePublicKey(key) {
     return sjcl.codec.base64.fromBits(key.x.concat(key.y))
 }
+
+
 function unserializePublicKey(key) {
     return new sjcl.ecc.elGamal.publicKey(
         sjcl.ecc.curves.c256,
